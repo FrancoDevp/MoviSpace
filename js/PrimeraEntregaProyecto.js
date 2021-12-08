be4ff05adf9a0af546c136f1df4e93e688553c8b
FrancoDevp <franco.cantonati.99@gmail.com>
2021-12-08 13:25:24 -0300

    mejoramos la interfas y agregamos un localStorage

--- js/PrimeraEntregaProyecto.js
+++ js/PrimeraEntregaProyecto.js
@@ -1,39 +1,18 @@
-// class PlanesMobiles {
-//     constructor (pack5, pack8, pack15) {
-//        this.pack5 = pack5;
-//        this.pack8 = pack8;
-//        this.pack15 = pack15;
-//      }
-//        mensajePrecio(){
-//          console.log(planElegido)
-//        switch (planElegido) {
-//          case "pack5": {
-//            alert("Este plan tiene el costo de $" + this.pack5)
-//          } break;
-//          case "pack8": {
-//            alert("Este plan tiene el costo de $" + this.pack8)
-//          } break;
-//          case "pack15": {
-//            alert("Este plan tiene el costo de $" + this.pack15)
-//          } break;
-//        }
-//        alert("Su plan elegido es el " + planElegido)
-//     }
-// }
 
-// let planesMobiles1 = new PlanesMobiles (350, 650, 1850);
-//    console.log(planesMobiles1);
-//    alert("Bienvenido a ¡MoviSpace!")
+const inputNombre = document.getElementById("nombre")
+const inputEmail = document.getElementById("email")
+let formularioCompra = document.getElementById("formulario");
+formularioCompra.addEventListener("submit", validarFormulario);
+function validarFormulario(e){
+    e.preventDefault();
+    localStorage.setItem("formulario", inputNombre.value + ", " + inputEmail.value)
+    inputNombre.value = "";
+    inputEmail.value = "";
+}
 
-// let planElegido = prompt("Tenemos todos tipos de planes, elige el que mas te guste: \n5Gb - 8Gb - 15Gb.\nEj: pack8.")
-// planesMobiles1.mensajePrecio();
-
-
-    
-
-// -----------------------------
 let precios = [150,150,300,450,0,300,300,450,450,150,150,300,450,450,300,300,150,600,150,750,300,300];
 const provincias = ["buenos aires", "catamarca", "chaco", "chubut", "cordoba", "corrientes", "formosa", "jujuy", "la pampa", "la rioja", "mendoza", "misiones", "neuquen", "rio negro", "salta", "san juan", "san luis", "santa cruz", "santa fe", "santiago del estero", "tierra del fuego", "tucuman"];
+localStorage.setItem("ArrayDeProincias", provincias)
 
 const localidadesDom = document.getElementById("provincias")
 for (let i = 0; i < provincias.length; i++) {
@@ -41,9 +20,21 @@ for (let i = 0; i < provincias.length; i++) {
     option.innerHTML = provincias[i]
     option.value = precios[i]
     localidadesDom.appendChild(option)
+    localStorage.setItem("optionProvincias", option.innerHTML + option.value)
 }
 
 localidadesDom.addEventListener("change", function (e) {
     let total = document.getElementById("total")
     total.innerHTML = "Total: $" + (Number(e.target.value) + Number(350))
 });
+
+
+
+
+// const nombre = "Franco";
+// localStorage.setItem("NombreApellido", nombre)
+
+// const localnombre = localStorage.getItem("NombreApellido")
+// console.log(localnombre);
+
+
