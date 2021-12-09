92de7c5e0d9223371d3f4e41c5d0129e496982c2
FrancoDevp <franco.cantonati.99@gmail.com>
2021-12-08 21:49:21 -0300

    terminamos segunda parte

--- js/PrimeraEntregaProyecto.js
+++ js/PrimeraEntregaProyecto.js
@@ -12,7 +12,7 @@ function validarFormulario(e){
 
 let precios = [150,150,300,450,0,300,300,450,450,150,150,300,450,450,300,300,150,600,150,750,300,300];
 const provincias = ["buenos aires", "catamarca", "chaco", "chubut", "cordoba", "corrientes", "formosa", "jujuy", "la pampa", "la rioja", "mendoza", "misiones", "neuquen", "rio negro", "salta", "san juan", "san luis", "santa cruz", "santa fe", "santiago del estero", "tierra del fuego", "tucuman"];
-localStorage.setItem("ArrayDeProincias", provincias)
+localStorage.setItem("ArrayDeProincias", JSON.stringify(provincias));
 
 const localidadesDom = document.getElementById("provincias")
 for (let i = 0; i < provincias.length; i++) {
@@ -20,13 +20,31 @@ for (let i = 0; i < provincias.length; i++) {
     option.innerHTML = provincias[i]
     option.value = precios[i]
     localidadesDom.appendChild(option)
-    localStorage.setItem("optionProvincias", option.innerHTML + option.value)
 }
+
+let select = document.getElementById("provincias")
+select.addEventListener('change',function(){
+    localStorage.setItem("costoEnvio", select.value)
+});
 
 localidadesDom.addEventListener("change", function (e) {
     let total = document.getElementById("total")
-    total.innerHTML = "Total: $" + (Number(e.target.value) + Number(350))
+    total.innerHTML = "Total: $" + (Number(e.target.value) + Number(localStorage.getItem("costoEnvio")))
 });
+
+(function(){
+
+    $(function(){
+
+        $('#open-modal').on('click',function() {
+            $('#modal').modal();
+        })
+
+    });
+
+}());
+
+
 
 
 
