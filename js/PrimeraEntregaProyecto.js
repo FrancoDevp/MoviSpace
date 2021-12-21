4cc3c138ae821db0924fc976bb85ca1683e80564
FrancoDevp <franco.cantonati.99@gmail.com>
2021-12-20 23:14:25 -0300

    modificaciones

--- js/PrimeraEntregaProyecto.js
+++ js/PrimeraEntregaProyecto.js
@@ -4,7 +4,7 @@ let email = document.getElementById("email");
 const inputEmail = document.getElementById("email")
 let formularioCompra = document.getElementById("formulario");
 formularioCompra.addEventListener("submit", validarFormulario);
-function validarFormulario(e){
+function validarFormulario(e) {
     e.preventDefault();
     localStorage.setItem("formulario", nombreApellido.value + ", " + inputEmail.value)
     nombreApellido.value = "";
@@ -14,7 +14,7 @@ function validarFormulario(e){
 let pack3g = 350;
 let pack5g = 650;
 let pack10g = 1250;
-let precios = [150,150,300,450,0,300,300,450,450,150,150,300,450,450,300,300,150,600,150,750,300,300];
+let precios = [150, 150, 300, 450, 0, 300, 300, 450, 450, 150, 150, 300, 450, 450, 300, 300, 150, 600, 150, 750, 300, 300];
 const provincias = ["buenos aires", "catamarca", "chaco", "chubut", "cordoba", "corrientes", "formosa", "jujuy", "la pampa", "la rioja", "mendoza", "misiones", "neuquen", "rio negro", "salta", "san juan", "san luis", "santa cruz", "santa fe", "santiago del estero", "tierra del fuego", "tucuman"];
 localStorage.setItem("ArrayDeProincias", JSON.stringify(provincias));
 
@@ -27,7 +27,7 @@ for (let i = 0; i < provincias.length; i++) {
 }
 
 let select = document.getElementById("provincias")
-select.addEventListener('change',function(){
+select.addEventListener('change', function () {
     localStorage.setItem("costoEnvio", select.value)
 });
 
@@ -37,61 +37,52 @@ localidadesDom.addEventListener("change", function (e) {
 });
 
 
-(function(){
-    
-    $(function(){
+(function () {
 
-        $('#open-modal').on('click',function() {
+    $(function () {
+
+        $('#open-modal').on('click', function () {
             $('#modal').modal();
         })
 
     });
-    
+
 }());
-    $("#valorEnvio").append("Valor del envio")
-    $("#flecha").css({"left": "20px"})
-    // 
-    $("#nombreApellido").mouseenter( function() {
-        $("#nombreApellido").css({"background-color": "#91d9f1"})
-    })
-    $("#email").mouseenter( function() {
-        $("#email").css({"background-color": "#91d9f1"})
-    })
-    $("#nombreApellido").mouseleave( function() {
-        $("#nombreApellido").css({"background-color": "white"})
-    })
-    $("#email").mouseleave( function() {
-        $("#email").css({"background-color": "white"})
-    })
-    // 
-    
+$("#valorEnvio").append("Valor del envio")
+$("#flecha").css({ "left": "20px" })
+// 
+$("#nombreApellido").mouseenter(function () {
+    $("#nombreApellido").css({ "background-color": "#91d9f1" })
+})
+$("#email").mouseenter(function () {
+    $("#email").css({ "background-color": "#91d9f1" })
+})
+$("#nombreApellido").mouseleave(function () {
+    $("#nombreApellido").css({ "background-color": "white" })
+})
+$("#email").mouseleave(function () {
+    $("#email").css({ "background-color": "white" })
+})
 
 
+let enviarBoton = document.getElementById("open-modal");
+console.log(nombreApellido);
 
-    let enviarBoton = document.getElementById("open-modal");
-    console.log(nombreApellido);
-
-function habilitar(){
-    if(nombreApellido.value !== '' && email.value !== '') {
-        enviarBoton.disabled = false; 
+function habilitar() {
+    if (nombreApellido.value !== '' && email.value !== '') {
+        enviarBoton.disabled = false;
     } else {
         enviarBoton.disabled = true;
     }
-    
-
-} 
-
+}
 
 nombreApellido.addEventListener("blur", habilitar);
 email.addEventListener("blur", habilitar);
-document.getElementById("open-modal").addEventListener("click", () => {});
+document.getElementById("open-modal").addEventListener("click", () => { });
 
 
 
-// const nombre = "Franco";
-// localStorage.setItem("NombreApellido", nombre)
+// Pagina LOGIN
 
-// const localnombre = localStorage.getItem("NombreApellido")
-// console.log(localnombre);
 
 
